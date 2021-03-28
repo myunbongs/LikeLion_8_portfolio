e9299f516627ad0e75a246af400ae0f5a7e58811
GyeongEun Kim <kekim20@naver.com>
2021-03-28 23:56:35 +0900

    first

--- LikeLion_PF/LikeLion_PF/settings.py
+++ LikeLion_PF/LikeLion_PF/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',
-    'accounts',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
--- LikeLion_PF/accounts/models.py
+++ LikeLion_PF/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Account (models.Model) :
+    email=models.CharField(max_length=100)
+    name=models.CharField(max_length=10)
+    userid=models.CharField(max_length=10)
+    password=models.CharField(max_length=10)
+
+class Meta :
+    db_table="accounts"
