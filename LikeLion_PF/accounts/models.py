5407358aaf275b4de88beaf37b6c5f1e5122cf4b
GyeongEun Kim <kekim20@naver.com>
2021-03-31 02:54:47 +0900

    모델 뷰 수정& login/out/signup 확인

--- LikeLion_PF/accounts/models.py
+++ LikeLion_PF/accounts/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 class Account (models.Model) :
-    email=models.CharField(max_length=100)
-    name=models.CharField(max_length=10)
-    userid=models.CharField(max_length=10)
-    password=models.CharField(max_length=10)
-
+    userid=models.CharField(max_length=10,verbose_name='ID')
+    email=models.CharField(max_length=100,verbose_name='email')
+    username=models.CharField(max_length=10,verbose_name='username')
+    password=models.CharField(max_length=10,verbose_name='password')
+    
 class Meta :
     db_table="accounts"
