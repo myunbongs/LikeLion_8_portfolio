5eca51368fd6e5ad7e30b84ae57dc14209120e0f
sooziini <soojinpar1026@gmail.com>
2021-09-02 19:56:14 +0900

    오타 수정

--- LikeLion_PF/accounts/forms.py
+++ LikeLion_PF/accounts/forms.py
@@ -7,15 +7,15 @@ class ProfileForm(forms.ModelForm):
         widget=forms.TextInput(attrs={
             'class': 'form-control',
             'autofocus':'True',
-            'placeholder': "닉네임을 입력해 주제요",
+            'placeholder': "닉네임을 입력해 주세요",
             'style':'margin: 10px auto; width:50%;',
         })
     )
     description = forms.CharField(
-        label="한 줄 소개:",
+        label="한줄 소개:",
         widget=forms.Textarea(attrs={
             'class': 'form-control',
-            'placeholder': "한 줄 소개를 입력해주세요",
+            'placeholder': "한줄 소개를 입력해 주세요",
             'style':'margin: 10px auto; width:50%;',
             'cols': 50, 'rows': 13
         })
